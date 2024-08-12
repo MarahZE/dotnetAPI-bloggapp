4dfca8a3e1cae3b57d129488e5bc6184d238bae5
maze6343 <maze6343@student.su.se>
2024-08-12 14:43:02 +0200

    try to create connection with SQL Server

---
+++ backend/Data/ApplicationsDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class ApplicationsDbContext : DbContext
+    {
+        public ApplicationsDbContext(DbContextOptions<ApplicationsDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b7e24e891cf4bc5de4616f3ce17864adb47e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96db0904042e3d60dd509738d9b747a8102f1dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
